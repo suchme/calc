a4efaa05d7921e35c223128a3140ea7c4b7bfaef
suchme <onono36@hotmail.com>
2021-07-31 00:37:33 +0900

    fix

--- src/main.js
+++ src/main.js
@@ -8,34 +8,48 @@ var binder = new Binder();
 
 v.addEventListener("seeked",function(e){
 	var old = calc.frame;
-	var frame = v.currentTime * calc.fps;
-	calc.frame =frame|0;
-	calc.frame +=Math.sign(old-frame);
+//	var frame = v.currentTime * calc.fps;
+		calc.frame =v.currentTime;
+	
+//	calc.frame +=Math.sign(old-frame);
 });
 window.main={};
+var presson = false;
 main.changeFrame = function(f){
-	calc.frame=Math.min(Math.max(calc.frame+f,0),v.duration*calc.fps);
-	v.currentTime=calc.frame/calc.fps;
+	var f2 = f;
+	if(presson){
+		f2=f*5;
+	}
+	if(isNaN(v.duration)){
+		return;
+	}
+	calc.frame=Math.min(Math.max(calc.frame+f2/calc.fps,0),v.duration);
+	v.currentTime=calc.frame;
+	presson=true;
 
-	cancel=window.setTimeout(()=>{
+	window.setTimeout(()=>{
+		if(!presson){
+			return;
+		}
 		main.changeFrame(f)}
-		,200
+		,400
 	);
+	
 }
 main.changeSec = function(s){
 	main.changeFrame(s*calc.fps);
 }
 main.recalc=function(){
-	calc.recv = (calc.bst0 - calc.bst1)/calc.dt;
+	calc.recv = Math.abs(calc.bst0 - calc.bst1)/calc.dt;
 }
 main.recalc2=function(){
-	calc.dt2 = (calc.bst2 - calc.bst3)/calc.recv;
-	calc.extend = (calc.range/100) *8833 / dt2;
+	calc.dt2 = Math.abs(calc.bst2 - calc.bst3)/calc.recv;
+	calc.extend = (calc.range/100) *8833 / calc.dt2;
 }
 
 
 document.addEventListener("pointerup",()=>{
-	clearTimeout(cancel);
+	presson=false;
 });
 document.addEventListener("change",(e)=>{
 
@@ -44,9 +58,11 @@ document.getElementById("file").addEventListener("change",
 	function(e){
 		var file=this.files[0];
 		
+		calc.frame=0;
 		// FileReaderを生成
-				var fileReader = new FileReader();
+		var fileReader = new FileReader();
 		 
+		calc.filename=file.name;
 	
 		// 読み込み完了時の処理を追加
 		fileReader.onload = function() {
